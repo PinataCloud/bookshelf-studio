3be2b1b6c95c7000a57ef82066c4ed0955499ca8
Steve <stevedsimkins@gmail.com>
2024-09-13 19:35:27 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-	title: "Shelves",
+	title: "Bookshelf",
 	description: "Create and share shelves of your favorite books",
 };
 
